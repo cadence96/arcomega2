a6464acf607fd27b04908be86aec69c4f5df711e
Ricardo <marcorcp1@hotmail.com>
2013-10-08 18:00:49 -0500

    secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arcomega2::Application.config.secret_token = 'e8162c55c8k83bbf467fa959376bce70707d9be7ab34f9dc73e3dc24d1e79e3ff247e6e63d76e35c72ea85f7c2053332891266e0402303f3ebd108e99fe58c21'
